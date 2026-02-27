a53e6207daf984074d8a308e03275325ccf07c5d
Ash-jo121 <ashishkaloor@gmail.com>
2026-02-27 23:07:19 +0530

    Fix: Added code for circuit breaker

--- Design Patterns/CircuitBreaker.js
+++ Design Patterns/CircuitBreaker.js
@@ -1,20 +1,31 @@
-function circuitBreaker(fn,failureCount,timeThreshold){
-    let attempts = 0;
-    let timeSinceLastFailure = 0;
+function circuitBreaker(fn, failureCount, timeThreshold) {
+  let attempts = 0;
+  let timeSinceLastFailure = 0;
 
-    return function(...args){
-        let context = this;
-        if(attempts < failureCount){
-            const result = fn.apply(context,args);
-            attempts++;
-            return result;
-        }else{
-            if(timeSinceLastFailure === 0){
-                timeSinceLastFailure = Date.now();
-            }
+  return function (...args) {
+    if (isClosed) {
+      const diff = Date.now() - timeSinceLastFailure;
 
-            const diff = Date.now() - timeSinceLastFailure;
-            if(diff > )
-        }
+      if (diff > timeThreshold) {
+        isClosed = false;
+      } else {
+        console.error("Service unavailable");
+        return;
+      }
     }
+
+    try {
+      const result = fn(...args);
+      failures = 0;
+      return result;
+    } catch (error) {
+      failures++;
+      timeSinceLastFailure = Date.now();
+      if (failures >= failureCount) {
+        isClosed = true;
+      }
+
+      console.log("Error");
+    }
+  };
 }
